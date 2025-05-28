249cf3131a2cb9fb6ed20f18fcd745cba3d66f6d
noahv! <noahjacobv@gmail.com>
2025-05-28 17:11:00 +0800

    Updated Profile and Recipe models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 from django.urls import reverse
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255, "This field must contain at least 255 characters")
+        ]
+    )
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
 class Ingredient(models.Model):
     name = models.CharField(max_length = 100)
 
@@ -14,13 +28,20 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length = 100)
 
+    author = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="recipe"
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+
     def __str__(self):
         return '{}'.format(self.name)
 
     def get_absolute_url(self):
         return reverse('ledger:recipe', args=[self.pk])
 
-class RecipeIngredient(models.Model): #linking recipe to ingredient
+class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length = 50)
 
     ingredient = models.ForeignKey(
