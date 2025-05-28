9533a8b11fcf42c7e746be69d8e0fa9fbea16ba8
NoahJVV <152837418+NoahJVV@users.noreply.github.com>
2025-05-29 04:41:01 +0800

    Updated views & urls
     - Created RecipeCreateView and RecipeUploadView
     - Added urls for creating recipes and adding images

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeUploadView
 
 urlpatterns = [
+    path("recipe/add", RecipeCreateView.as_view(), name="add"),
     path("recipes/list", RecipeListView.as_view(), name="list"),
     path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="recipe"),
+    path("recipe/add_image", RecipeUploadView.as_view(), name="upload"),
+    path("recipe/<int:pk>/add_image", RecipeUploadView.as_view(), name="upload"),
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from ledger.models import Recipe
+from ledger.models import Recipe, RecipeIngredient, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 class RecipeListView(ListView):
     model = Recipe
@@ -12,3 +15,40 @@ class RecipeListView(ListView):
 class RecipeDetailView(DetailView, LoginRequiredMixin):
     model = Recipe
     template_name = "recipe_detail.html"
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = "recipe_add.html"
+
+class RecipeUploadView(CreateView):
+    model = Recipe
+    form_class = RecipeImageForm
+    template_name = "recipe_add_image.html"
+
+    def get_success_url(self):
+        return reverse_lazy("ledger:recipe", kwargs={"pk": self.object.pk})
+
+    def get(self, request, pk):
+        recipe = Recipe.objects.get(pk=pk)
+        form = RecipeImageForm()
+
+        ctx = {"recipe": recipe, "form": form}
+        
+        return render(request, "recipe_add_image.html", ctx)
+
+    def post(self, request, pk):
+        recipe = Recipe.objects.get(pk=pk)
+        form = RecipeImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            image = RecipeImage()
+            image.image = form.cleaned_data.get("image")
+            image.desc = form.cleaned_data.get("desc")
+            image.recipe_image = recipe
+            image.save()
+            return redirect("ledger:recipe", pk=recipe.pk)
+        
+        ctx = {"recipe": recipe, "form": form}
+
+        return render(request, "recipe_add.html", ctx)
